19e916ccce5994161e5512c85965691bec30bbf1
Angel Byun <abyun22@turing.edu>
2023-02-04 17:28:18 -0700

    Add append, next_node, count, and to_string method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,11 +1,18 @@
 class LinkedList
-    attr_reader :head
+    attr_reader :head,
+                :count
     def initialize
         @head = nil
+        @count = 0
     end
     def append(data)
+        @count += 1
         if @head == nil
-        @head = Node.new(data)
+            @head = Node.new(data)
         end
+    end
+
+    def to_string
+        @head.data
     end
 end
