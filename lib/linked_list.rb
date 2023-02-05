bbd38ab0e44b834b952fa15d8b020bd0085f18e0
Angel Byun <abyun22@turing.edu>
2023-02-05 15:43:31 -0700

    Modify append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,10 +6,14 @@ class LinkedList
         @count = 0
     end
     def append(data)
+        current_node = @head
         @count += 1
         if @head == nil
             @head = Node.new(data)
+        else @head.next_node = Node.new(data)
+            current_node = current_node.next_node
         end
+        data
     end
 
     def to_string
