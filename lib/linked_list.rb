b86b55771ef70b5b7f03c6948b07fe800449dba8
Angel Byun <abyun22@turing.edu>
2023-02-05 19:51:03 -0700

    Modify to_string method with loop method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -12,11 +12,24 @@ class LinkedList
             @head = Node.new(data)
         else @head.next_node = Node.new(data)
             current_node = current_node.next_node
+            while(!current_node.next_node.nil?)
+                current_node = current_node.next_node
+            end
         end
         data
     end
 
     def to_string
-        @head.data
+        to_string = []
+        if @head.nil?
+            to_string = []
+        else
+            current_node = @head
+            while(!current_node.nil?)
+                to_string << current_node.data
+                current_node = current_node.next_node
+            end
+        end
+        to_string.join(" ")
     end
 end
