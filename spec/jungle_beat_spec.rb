edef2fd8ddfbdef990b4dfad4a80aa8810a03503
Angel Byun <abyun22@turing.edu>
2023-02-07 15:07:04 -0700

    Add data test and head

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -5,8 +5,29 @@ require './lib/jungle_beat'
 
 RSpec.describe JungleBeat do
     it "exists" do
-    list = JungleBeat.new
+        list = JungleBeat.new
     
-    expect(list).to eq(JungleBeat)
+    expect(list).to be_instance_of(JungleBeat)
+    end
+
+    it "has a head" do
+        list = JungleBeat.new
+        list.head
+
+        expect(list.head).to eq(nil)
+    end
+
+    it "adds data" do
+        list = JungleBeat.new
+        list.append("deep doo ditt")
+
+        expect(list.head.data).to eq("deep doo ditt")
+    end
+
+    it "modifies data" do
+        list = JungleBeat.new
+        list.append("deep doo ditt")
+
+        expect(list.head.data).to eq("deep")
     end
 end
