302efc8c5dd5e155b07a9f349fc067d3aa5ece4e
Angel Byun <abyun22@turing.edu>
2023-02-07 16:21:43 -0700

    Modify append method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,23 +1,19 @@
 class JungleBeat
-    attr_reader :head
-                :count
+    attr_accessor   :list
+                    :count
 
     def initialize
-        @head = nil
+        @list = LinkedList.new
         @count = 0
     end
 
     def append(data)
-        current_node = @head
         @count += 1
-        if @head == nil
-            @head = Node.new(data)
-        else
-            new_node = @head
-            while(!new_node.next_node.nil?)
-                new_node = new_node.next_node
-            end
-            new_node.next_node = Node.new(data)
+        sounds = data.split
+        sounds.each do |sound|
+            # require 'pry'; binding.pry
+            list.append(sound)
         end
+        sounds.join(" ")
     end
 end
